df60c98d3d0a0c8770cdb68b5e6923f0ee7f3e22
aiiram <imariia@yahoo.com>
2023-05-15 12:14:46 +0200

    task1, 2

---
+++ script.js
@@ -0,0 +1,17 @@
+// Template string
+
+// const x = 50
+// const y = 30
+// if (x > y) {
+//     console.log (`${x} > ${y}`);
+// }
+
+// const color = 'blue' 
+// const newColor = 'green' 
+// if (color == newColor) {
+//     console.log ('Kolory się zgadzają');
+// }
+//     else {
+// console.log('Kolory się nie zgadzają')
+//     }
+
